fe668d28f31641330af637490089c7f798bf854b
Ruzhad9494 <ruzbihan@voltake.com>
2026-07-04 14:08:23 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/ompdashboard/',   // <- exact nama repo kau
   build: {
     outDir: 'dist'
   }
